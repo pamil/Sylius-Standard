fab6ad0b87005bdf6524b5058cc6430c24eb6802
Adam Kasperczak <adamkasperczak123@gmail.com>
2021-02-10 09:44:06 +0100

    Upgrade to Sylius v1.9.0-BETA.3 wih Symfony 4.4

--- config/bundles.php
+++ config/bundles.php
@@ -60,4 +60,5 @@ return [
     Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
     Sylius\PayPalPlugin\SyliusPayPalPlugin::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
 ];
